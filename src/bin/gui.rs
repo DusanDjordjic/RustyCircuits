e8dec56db26ae48408930367af7d005bca3a3830
DusanDjordjic <dukidjordjic@gmail.com>
2023-04-30 01:35:47 +0200

    Wip loading network components from stdin

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
+    let mut network: rusty_circuits::Network<u32> = rusty_circuits::Network::default();
+
+    if let Err(e) = rusty_circuits::loaders::stdin::start_menu(&mut network) {
+        eprintln!("{e}");
+    }
 }
---
+++ src/network/loaders.rs
@@ -0,0 +1,62 @@
+pub mod stdin {
+    use std::hash::Hash;
+    use std::io::Write;
+    use std::str::FromStr;
+
+    use crate::edge::Edge;
+    use crate::node::Node;
+    use crate::Network;
+
+    pub fn start_menu<K>(network: &mut Network<K>) -> std::io::Result<()>
+    where
+        K: Eq + Hash + Copy + FromStr,
+    {
+        let mut user_input = String::new();
+        let stdin = std::io::stdin();
+        let mut stdout = std::io::stdout();
+
+        'outer_loop: loop {
+            println!("1. Add Node");
+            println!("2. Add Edge");
+            println!("0. Quit");
+            print!("> ");
+
+            stdout.flush()?;
+
+            stdin.read_line(&mut user_input)?;
+
+            if user_input.starts_with('0') {
+                break 'outer_loop;
+            } else if user_input.starts_with('1') {
+                if let Err(e) = network.insert_node(load_node()) {
+                    eprintln!("{e}")
+                }
+            } else if user_input.starts_with('2') {
+                let u = select_node();
+                let v = select_node();
+                network.insert_egde(load_edge(u, v));
+            } else {
+                println!("Invalid input");
+            }
+        }
+        Ok(())
+    }
+
+    fn load_edge<K>(_u: Node<K>, _v: Node<K>) -> Edge<K>
+    where
+        K: FromStr,
+    {
+        unimplemented!()
+    }
+
+    fn load_node<K>() -> Node<K>
+    where
+        K: FromStr,
+    {
+        unimplemented!()
+    }
+
+    fn select_node<K>() -> Node<K> {
+        unimplemented!()
+    }
+}
